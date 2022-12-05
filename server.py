7496c23a56f48f0fe0d6b6053808e440971aa58d
tusharad <tusharadhatrao@gmail.com>
2022-12-05 12:58:57 +0530

    Implemented download text with timestamp and caret

--- server.py
+++ server.py
@@ -49,13 +49,13 @@ def home_app(response):
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <title>Hindi AutoComplete</title>
 </head>
 <body>
 <div class="container">
     <h1 class="text-center text-primary mt-3 mb-3">Hindi AutoComplete using SQLite3</h1>
-    <button  type="button" class="btn btn-primary" href="" id="a">Download</button>
-    <button type="button" class="btn btn-warning" onclick="download()">Save</button>
+<button class="btn btn-primary" onclick=saveTextAsFile(input.value)>Download</button>
         <textarea rows="4" style="border:solid black 1px" id="input" name="query" class="form-control form-control-lg">
          
         </textarea>
@@ -64,17 +64,41 @@ def home_app(response):
         <span id="suggestions"></span>
  </div>
  <script type="text/javascript">
-    const search_area = document.querySelector('#input')
-    search_area.addEventListener('input',function handleChange(e){
-        data = e.target.value;
-         if(data[data.length - 1] == " "){
+
+
+
+   function getCaretPosition(ctrl) {
+    var start, end;
+    if (ctrl.setSelectionRange) {
+        start = ctrl.selectionStart;
+        end = ctrl.selectionEnd;
+    } else if (document.selection && document.selection.createRange) {
+        var range = document.selection.createRange();
+        start = 0 - range.duplicate().moveStart('character', -100000);
+        end = start + range.text.length;
+    }
+    return {
+        start: start,
+        end: end
+    }
+}
+
+$("textarea").on("click keyup", function () {
+    var caret = getCaretPosition(this);
+    var endPos = this.value.indexOf(' ',caret.end);
+    if (endPos ==-1) endPos = this.value.length;
+    var result = /\S+$/.exec(this.value.slice(0, endPos));
+    var lastWord = result ? result[0] : null;
+    if (lastWord) lastWord = lastWord.replace(/['";:,.\/?\\-]$/, ''); // remove punctuation
+   console.log(lastWord)
+   var word = lastWord;
+if(word){
             storeClientCache(word);
             getData(word);
-            }else{
-                word = data.split(" ").pop().trim();
-                getData(word);
-            }
-    });
+       }     
+
+});
+
 
     function getData(query) {
 		fetch('/search?query=' + query + '').then(function (response) {
@@ -83,7 +107,7 @@ def home_app(response):
 
             var html = '<ul class="list-group" >';
 			for(var cnt = 0;cnt < freqMap.length;cnt++){
-                if (freqMap[cnt].substr(0, word.length) == word && word != ""){
+                if (freqMap[cnt].substr(0, query.length) == query && query != ""){
                     console.log("from client console");
 					var regular_expression = new RegExp('(' + query + ')', 'gi');
 					html += '<a href="#" style="color:red" class="list-group-item list-group-item-action" onclick="get_text(this)">' + freqMap[cnt].replace(regular_expression, '<span class="text-primary ">$1</span>') + '</a>';
@@ -105,6 +129,7 @@ def home_app(response):
 	}
 
     function storeClientCache(query) {
+        console.log("storing");
             var stringObj = localStorage['wordFreq'] || '[]';
             var freqMap = { arr: [] };
             if (stringObj) {
@@ -126,14 +151,39 @@ def home_app(response):
 		document.getElementById('input').focus();
 	}
 
-    function download() {
-  var a = document.getElementById("a");
-  const content = document.getElementById('input').value;
-  var file = new Blob([content], {type: 'txt'});
-  a.href = URL.createObjectURL(file);
-  const d = new Date();
-  a.download = d;
-}
+function saveTextAsFile(textToWrite, fileNameToSaveAs)
+    {
+    	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'}); 
+    	var downloadLink = document.createElement("a");
+   var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds() + ".txt";
+
+    	downloadLink.download = datetime;
+    	downloadLink.innerHTML = "Download File";
+    	if (window.webkitURL != null)
+    	{
+    		// Chrome allows the link to be clicked
+    		// without actually adding it to the DOM.
+    		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    	}
+    	else
+    	{
+    		// Firefox requires the link to be added to the DOM
+    		// before it can be clicked.
+    		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    		downloadLink.onclick = destroyClickedElement;
+    		downloadLink.style.display = "none";
+    		document.body.appendChild(downloadLink);
+    	}
+    
+    	downloadLink.click();
+    }
+
 </script>
 
 </body>
